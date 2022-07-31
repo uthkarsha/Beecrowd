30d46364a12c3c33ef662346a632f4447a58ce61
uthkarsha <uthbar@gmail.com>
2022-07-31 19:52:33 +0600

    beecrowd 1014,1015,1016 added

---
+++ 1014.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+
+{
+
+    double X;
+
+    double Y;
+
+    cin >> X >> Y;
+
+    cout << fixed << setprecision(3) << X / Y << " km/l"  << endl;
+
+    return 0;
+
+}
+
+
---
+++ 1016.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int d;
+    cin >> d;
+    int t = d*2;
+    cout << t << " minutos" << endl;
+    return 0;
+}
